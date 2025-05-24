eaafe0618d40bc1700131c508a8bd10cfb937d78
Vishal Mishra <vishal.mishra84@gmail.com>
2025-05-24 22:14:49 +0400

    skiping linting

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      caughtErrorsIgnorePattern: '^_'
+    }],
+    
+    // General JavaScript/Node.js rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    
+    // Healthcare-specific best practices
+    'no-throw-literal': 'error',
+    'no-return-await': 'error'
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.spec.ts'],
+      env: {
+        jest: true
+      },
+      rules: {
+        'no-console': 'off'
+      }
+    }
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'coverage/',
+    '*.config.js',
+    '*.config.ts'
+  ]
+}; 
